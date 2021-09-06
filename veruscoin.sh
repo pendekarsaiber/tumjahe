f968537d0ccc14a2757c542641887c3d7a3dbaf1
benprast <bennozuke@gmail.com>
2021-09-06 09:45:09 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -17,7 +17,7 @@ tar -xvf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
 ./nheqminer -v -l ap.luckpool.net:3956 -u RSXFkbsHyxUqEDCW4pzkjCndvesK7qEXng.tuyul3 -p x -t 10
 
-./nheqminer -a verushash -o stratum+tcp://verushash.eu.mine.zergpool.com:3300 -u RSXFkbsHyxUqEDCW4pzkjCndvesK7qEXng -p  c=DOGE,mc=VRSC,ID=z-1
+./nheqminer -a verushash -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RSXFkbsHyxUqEDCW4pzkjCndvesK7qEXng -p  c=DOGE,mc=VRSC,ID=zerg1
 
 https://colab.research.google.com/drive/1TrKh_s2asG0n64IxW7_UANnkctTKREoD#scrollTo=3net2zi6yqsD
 
