45af2cd431853a247517c4eadfe986bcc62894dc
benprast <bennozuke@gmail.com>
2021-09-07 11:43:44 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -30,6 +30,7 @@ https://colab.research.google.com/drive/1VKAOqvzqbf5lhPYI_Gecp0OtaGF62j8E
 
 wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-x64.tar.gz
 tar -xvf xmrig-6.15.0-linux-x64.tar.gz
+cd xmrig-6.15.0/
 
 
 
