a213818274b61df0ebafb74cf113f8589ebb7b1d
pendekarsaiber <90118379+pendekarsaiber@users.noreply.github.com>
2021-09-13 19:22:18 +0700

    Update veruscoin1.sh

--- veruscoin1.sh
+++ veruscoin1.sh
@@ -8,3 +8,31 @@ while [ 1 ]; do
 sleep 3
 done
 sleep 999
+
+===================mining verus di hp======================
+Install dulu linux ubuntu di termux
+Cara install nya https://zee.gl/qbgkev
+
+1 cara instalasi ccminer di termux
+
+ apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential 
+
+apt-get install git
+
+git clone --single-branch -b ARM https://github.com/monkins1010/ccmine... 
+
+ cd ccminer 
+
+ chmod +x build.sh
+
+ chmod +x configure.sh 
+
+ chmod +x autogen.sh 
+
+./build.sh
+
+ Then To Run the miner do the following branch Program Algo Pool User Password threads devices ARM
+
+./ccminer  -a verus  -o stratum+tcp://eu.luckpool.net:3956  -u  RMHCNyJjNwcU9pTHqjzcmVtBm1x1PE95t2.kris07  -p x  -t 2
+
+
